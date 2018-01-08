1231615569363c5401d994133926c96225571aa0
dex.papa@gmail.com <dex.papa@gmail.com>
2018-01-08 20:05:30 +0530

    Expected output
    Getting Expected output as proper JSON, need code clean up

--- make-api-php/index.php
+++ make-api-php/index.php
@@ -3,7 +3,9 @@
 
     $mysqli = mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
     $query = "select * from admins";
+    $rows = array();
     $result = mysqli_query($mysqli,$query);
-    while($addrow = mysqli_fetch_array($result)){
-        echo json_encode($addrow,JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    while($addrow = mysqli_fetch_assoc($result)){
+        $rows[] = $addrow;
     }
+    print json_encode($rows,JSON_PRETTY_PRINT);
